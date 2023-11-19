9f74396d8895e659ea977960a6270e4a09f503a8
Brian <galyenb@vcu.edu>
2023-11-19 13:17:58 -0500

    added sequelize and sync to server.js

--- seeds/index.js
+++ seeds/index.js
@@ -1,24 +1,24 @@
-const seedCategories = require('./category-seeds');
-const seedProducts = require('./product-seeds');
-const seedTags = require('./tag-seeds');
-const seedProductTags = require('./product-tag-seeds');
+const seedCategories = require("./category-seeds");
+const seedProducts = require("./product-seeds");
+const seedTags = require("./tag-seeds");
+const seedProductTags = require("./product-tag-seeds");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
-  console.log('\n----- DATABASE SYNCED -----\n');
+  console.log("\n----- DATABASE SYNCED -----\n");
   await seedCategories();
-  console.log('\n----- CATEGORIES SEEDED -----\n');
+  console.log("\n----- CATEGORIES SEEDED -----\n");
 
   await seedProducts();
-  console.log('\n----- PRODUCTS SEEDED -----\n');
+  console.log("\n----- PRODUCTS SEEDED -----\n");
 
   await seedTags();
-  console.log('\n----- TAGS SEEDED -----\n');
+  console.log("\n----- TAGS SEEDED -----\n");
 
   await seedProductTags();
-  console.log('\n----- PRODUCT TAGS SEEDED -----\n');
+  console.log("\n----- PRODUCT TAGS SEEDED -----\n");
 
   process.exit(0);
 };
