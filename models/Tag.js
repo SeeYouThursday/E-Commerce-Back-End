22673ae6c011a2e3c6e232d075b0b5b6c14c3dbb
Brian <galyenb@vcu.edu>
2023-11-18 20:23:09 -0500

    added associations between models TODO: Routes

--- models/Tag.js
+++ models/Tag.js
@@ -1,12 +1,22 @@
 const { Model, DataTypes } = require("sequelize");
 
 const sequelize = require("../../config/connection.js");
+const Product = require("./Product.js");
 
 class Tag extends Model {}
 
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
--- models/index.js
+++ models/index.js
@@ -1,16 +1,26 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
 // Categories have many Products
-
+Category.hasMany(Products, {
+  foreignKey: "category_id",
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  foreignKey: "product_id",
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey: "tag_id",
+});
 
 module.exports = {
   Product,
