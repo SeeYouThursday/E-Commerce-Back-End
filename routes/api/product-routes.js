e892ffb57e53735c3d125571b2496761f8c343c9
Brian <galyenb@vcu.edu>
2023-11-19 15:41:26 -0500

    product routes completed and passed tests in insomnia

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -18,17 +18,33 @@ router.get("/", async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
-
-  // be sure to include its associated Category and Tag data
 });
 
-// get one product
-router.get("/:id", (req, res) => {
+// get one product -- passed insomnia test
+router.get("/:id", async (req, res) => {
   // find a single product by its `id`
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [
+        { model: Category },
+        //to include associated data which is THROUGH the ProductTag table
+        { model: Tag, as: "product_tags" },
+      ],
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: "No product found with that id!" });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Category and Tag data
 });
 
-// create new product
+// create new product --passed insomnia test
 router.post("/", (req, res) => {
   /* req.body should look like this...
     {
@@ -104,8 +120,24 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: "No product found with that id!" });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
